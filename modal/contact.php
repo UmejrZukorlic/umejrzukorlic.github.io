16236f2ccfda82fd2292fedf1f0b9de7b0b14d2a
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2026-04-26 17:38:28 +0200

    feat: setting downloda cv button

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "umejrzuka@gmail.com";
+$php_main_email = "zukorlicumejr@gmail.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
